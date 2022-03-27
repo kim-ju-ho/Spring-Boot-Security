66ba03731c31dddb337c6321e577b7ed3bb576cd
kim-ju-ho <kim1jo2ho3@naver.com>
2022-03-28 01:36:44 +0900

    template 수ì , 접근 권한 수정

--- src/main/java/com/sparta/springdetailpersonaltraining/controller/HomeController.java
+++ src/main/java/com/sparta/springdetailpersonaltraining/controller/HomeController.java
@@ -1,19 +1,19 @@
 package com.sparta.springdetailpersonaltraining.controller;
 
-import com.sparta.springdetailpersonaltraining.security.UserDetailsImpl;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
-    @GetMapping("/")
-    public String home(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails){
-        if(userDetails.getUser() !=null){
-            model.addAttribute("username", userDetails.getUsername());
-        }
 
-        return "index.html";
+    @GetMapping("/")
+    public String home(){
+//        if(userDetails.getUser() !=null){
+//            model.addAttribute("username", userDetails.getUsername());
+//        }
+
+        return "index";
     }
 }
